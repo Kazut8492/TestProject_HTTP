b84d2bf568b788ae0ddafa6962ddb7d990578b8c
Kazuki Taira <kazuki@lift99.co>
2022-12-13 16:40:29 +0200

    more http basics

--- server.js
+++ server.js
@@ -14,7 +14,30 @@ app.use(express.urlencoded({ extended: false }));
 // })
 
 app.post('/contact', (req, res)=>{
-    res.send(req.body.name);
+    if(!req.body.name) {
+        return res.status(400).send('Name is required');
+    }
+    // Database stuff
+    res.status(200).send('Thank you for contacting us, ' + req.body.name);
+})
+
+app.post('/login', (req, res)=>{
+    if(!req.header('x-auth-token')) {
+        return res.status(400).send('No token provided');
+    }
+    if(req.header('x-auth-token') !== '12345') {
+        return res.status(401).send('Invalid token');
+    }
+    res.send('Logged in')
+})
+
+app.put('/post/:id', (req, res)=>{
+    // Database stuff
+
+    res.json({
+        id: req.params.id,
+        title:req.body.title
+    });
 })
 
 app.listen(5001, ()=>console.log('Server started on port 5001'));
